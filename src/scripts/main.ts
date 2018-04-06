ad1f8f4329e2887f68fbca53e5b78338c9846d4e
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-04-05 20:20:30 -0700

    Added Injector and SymbolType.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -5,3 +5,26 @@ export function run() {
 window.onload = function () {
     console.log(run());
 }
+
+let typeHash: {
+    [index: string]: Symbol;
+} = {};
+
+class SymbolType<T> {
+    symbol: Symbol;
+    constructor(name: string) {
+        this.symbol = Symbol(name);
+        this.symbol.toString
+    }
+}
+function createSymbol(name: string) {
+    return Symbol(name);
+}
+
+
+
+class Injector {
+    typeHash: {
+        [index: string]: Symbol;
+    } = {};
+}
