1075332f6af447b2585a484edd4db82bf6669e25
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-04-09 10:59:36 -0700

    Cleaned main file and package-lock.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,28 +1,3 @@
 export { default as ParameterInfo } from './ParameterInfo';
 export { inject, injectable, factory } from './Decorators';
 export { default as InjectionContext } from './InjectionContext';
-
-let typeHash: {
-    [index: string]: Symbol;
-} = {};
-
-class SymbolType<T> {
-    symbol: Symbol;
-    constructor(name: string) {
-        this.symbol = Symbol(name);
-        this.symbol.toString
-    }
-}
-function createSymbol(name: string) {
-    return Symbol(name);
-}
-
-
-
-class Injector {
-    typeHash: {
-        [index: string]: Symbol;
-    } = {};
-}
-
-
